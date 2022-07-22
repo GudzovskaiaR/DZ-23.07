4fcc517380659f8fc8dbf7a0bb5381c378794fca
GudzovskaiaR <100199861+GudzovskaiaR@users.noreply.github.com>
2022-07-22 12:31:31 +0300

    add tasks 14-20

--- scripts/main.js
+++ scripts/main.js
@@ -131,3 +131,159 @@
     
 //   ];
 // -------------------------------------------------------------------------------------------------
+// A foods object has been created with three entries. Using the syntax of your choice, add three more entries to it: bananas with a value of 13, grapes with a value of 35, and strawberries with a value of 27.
+
+// let foods = {
+//     apples: 25,
+//     oranges: 32,
+//     plums: 28
+//   };
+//   foods.bananas = 13;
+//   foods["grapes"] = 35;
+//   const item = 'strawberries';
+//   foods[item] = 27;
+  
+//   console.log(foods);
+
+// --------------------------------------------------------------------------------------------------------------------
+// Here we've defined an object userActivity, which includes another object nested within it. Set the value of the online key to 45.
+
+// let userActivity = {
+//     id: 23894201352,
+//     date: 'January 1, 2017',
+//     data: {
+//       totalUsers: 51,
+//       online: 42
+//     }
+//   };
+//   userActivity.data.online = 45
+  
+  
+//   console.log(userActivity);
+// ------------------------------------------------------------------------------------------------------------------------
+
+// We've defined a function, checkInventory, which receives a scanned item as an argument. Return the current value of the scannedItem key in the foods object. You can assume that only valid keys will be provided as an argument to checkInventory.
+
+// let foods = {
+//     apples: 25,
+//     oranges: 32,
+//     plums: 28,
+//     bananas: 13,
+//     grapes: 35,
+//     strawberries: 27
+//   };
+  
+//   function checkInventory(scannedItem) {
+//     return foods[scannedItem]
+//   }
+  
+//   console.log(checkInventory("apples"));
+// -------------------------------------------------------------------------------------------------------------
+
+// Use the delete keyword to remove the oranges, plums, and strawberries keys from the foods object
+
+// let foods = {
+//     apples: 25,
+//     oranges: 32,
+//     plums: 28,
+//     bananas: 13,
+//     grapes: 35,
+//     strawberries: 27
+//   };
+//   delete foods.oranges;
+//   delete foods.plums;
+//   delete foods.strawberries;
+  
+  
+//   console.log(foods);
+// -------------------------------------------------------------------------------------------------------------------
+
+// Finish writing the function so that it returns true if the object passed to it contains all four names, Alan, Jeff, Sarah and Ryan and returns false otherwise.
+
+// let users = {
+//     Alan: {
+//       age: 27,
+//       online: true
+//     },
+//     Jeff: {
+//       age: 32,
+//       online: true
+//     },
+//     Sarah: {
+//       age: 48,
+//       online: true
+//     },
+//     Ryan: {
+//       age: 19,
+//       online: true
+//     }
+//   };
+  
+//   function isEveryoneHere(userObj) {
+   
+//     return (userObj.hasOwnProperty('Alan') && 
+//      userObj.hasOwnProperty('Jeff') && 
+//      userObj.hasOwnProperty('Sarah') && 
+//      userObj.hasOwnProperty('Ryan'))
+      
+//      }
+//   console.log(isEveryoneHere(users));
+// ------------------------------------------------------------------------------------------------------------------
+
+// We've defined a function countOnline which accepts one argument (a users object). Use a for...in statement within this function to loop through the users object passed into the function and return the number of users whose online property is set to true. An example of a users object which could be passed to countOnline is shown below. Each user will have an online property with either a true or false value.
+
+// const users = {
+//     Alan: {
+//       online: false
+//     },
+//     Jeff: {
+//       online: true
+//     },
+//     Sarah: {
+//       online: false
+//     }
+//   }
+  
+//   function countOnline(usersObj) {
+//     let result = 0;
+//     for (let user in usersObj) {
+//     if (usersObj[user].online === true) {
+//       result++
+//     }
+//   }
+//   return result
+//   }
+  
+//   console.log(countOnline(users));
+// ---------------------------------------------------------------------------------------------------------------------------
+
+// Finish writing the getArrayOfUsers function so that it returns an array containing all the properties in the object it receives as an argument.
+// let users = {
+//     Alan: {
+//       age: 27,
+//       online: false
+//     },
+//     Jeff: {
+//       age: 32,
+//       online: true
+//     },
+//     Sarah: {
+//       age: 48,
+//       online: false
+//     },
+//     Ryan: {
+//       age: 19,
+//       online: true
+//     }
+//   };
+  
+//   function getArrayOfUsers(obj) {
+     
+//      return Object.keys(obj)
+//   }
+  
+//   console.log(getArrayOfUsers(users));
+// --------------------------------------------------------------------------------------------------------------------------
+
+// Take a look at the object we've provided in the code editor. The user object contains three keys. The data key contains five keys, one of which contains an array of friends. From this, you can see how flexible objects are as data structures. We've started writing a function addFriend. Finish writing it so that it takes a user object and adds the name of the friend argument to the array stored in user.data.friends and returns that array.
+
